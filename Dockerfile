0653d35afa63149771b53c0bc35e0e1c39394a81
DavidLizarazo <47227841+DLeonardoL@users.noreply.github.com>
2023-07-12 15:07:00 -0500

    re estructuraciòn repo

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.11.0-alpine
-COPY . /app
-WORKDIR /app
-RUN pip install -r requirements.txt
-ENTRYPOINT ["python"]
-CMD ["app.py"]
-
-EXPOSE 80
--- app.py
+++ app.py
@@ -18,7 +18,5 @@ app.register_blueprint(inventarios)
 
 # EndBlueprints
 
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=80)
+
 
